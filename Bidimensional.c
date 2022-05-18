b68ac619457790730bb70d12b3612251c1f52222
FaustoSP <71281487+FaustoSP@users.noreply.github.com>
2022-05-18 19:21:27 -0300

    Primera implementacion bidimensional
    Siempre converge a un valor muy chico (alrededor de 0.2) y tarda una gran cantidad de iteraciones. Probablemente ambas cosas sean un bug.

--- Bidimensional.c
+++ Bidimensional.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<sys/time.h>
 #include <stdbool.h>
+#include <math.h>
 
 /**********Para calcular tiempo*************************************/
 double dwalltime()
@@ -15,11 +16,13 @@ double dwalltime()
 }
 /****************************************************************/
 
+//TODO: optimizar inicialización de la matriz (j primero, creo?)
 //Inicializa una matriz
 float* inicializarMatriz(float* m,unsigned long N){
 	for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
-	        m[i*N+j]=(rand()%10) + 1;
+	        m[i*N+j]=((float)rand())/RAND_MAX;
+            //printf("m[%i,%i]: %.2f\n", i, j, m[i*N+j]);
         }
     }
 	return m;
@@ -33,55 +36,104 @@ int main(int argc, char* argv[]){
         exit(1);
     }
 
-    float* mSecOri;
+    double timetick;
+
+    float* mSec;
     float* mSecConvergido;
     float* vPar;
     //Para verificar la convergencia se toma el primer elemento ( V[0] ) y se compara con el resto de los elementos del vector. Si la diferencia en valor
     //absoluto del valor del primer elemento con todos los elementos restantes es menor a un valor de precisión el algoritmo converge, en caso contrario el
     //algoritmo no converge.
     //TODO: preguntar de donde saco el valor de precisión
-    int valorDePrecisionDeConvergencia, i, iteraciones = 0;
+    int i, j, iteraciones = 0;
     bool convergio = false;
     unsigned long N = atol(argv[1]);
     unsigned long numBytes = sizeof(float)*N*N;
 
     //Se inicializan las matrices
-    mSecOri=(float*)malloc(numBytes);
-	mSecOri=inicializarMatriz(mSecOri,N);
+    mSec=(float*)malloc(numBytes);
+	mSec=inicializarMatriz(mSec,N);
 
     mSecConvergido=(float*)malloc(numBytes);
 
+    bool firstTime = true;
+    timetick = dwalltime();
     while(!convergio){
         iteraciones++;
-        //Se calcula el promedio entre cada valor y sus vecinos
-        for(i=0;i<N;i++){
-            if(i == 0){
-                mSecConvergido[i] = ((mSecOri[0] + mSecOri[1]) / 2);
-            } else {
-                if(i == N-1){
-                mSecConvergido[i] = ((mSecOri[N - 1] + mSecOri[N - 2]) / 2);
-                } else {
-                    mSecConvergido[i] = ((mSecOri[i - 1] + mSecOri[i] + mSecOri[i+1]) / 3);
-                }
+
+        //La posicion de un valor de la matriz se calcula como i*N+j
+
+        //Primero se calcula el valor de las esquinas. Notese que le indice maximo para cada variable es N-1, no N.
+        mSecConvergido[0] = ((mSec[0] + mSec[1] + mSec[N] + mSec[N+1]) / 4.0); //Esquina superior izquierda (0,0).
+        mSecConvergido[N-1] = ((mSec[(N-2*N)] + mSec[(N-1)*N] + mSec[(N-2)*N+1] + mSec[(N-1)*N+1]) / 4.0); //Esquina superior derecha (0,N-1).
+        mSecConvergido[(N-1)*N] = ((mSec[N-2] + mSec[N+N-2] + mSec[N-1] + mSec[N+N-1]) / 4.0); //Esquina inferior izquierda (N-1,0).
+        mSecConvergido[(N-1)*N+N-1] = ((mSec[(N-2)*N+N-2] + mSec[(N-1)*N+N-2] + mSec[(N-2)*N+N-1] + mSec[(N-1)*N+N-1]) / 4.0); //Esquina inferior derecha (N-1,N-1)
+
+        //Luego se calcula el valor de los bordes, menos las esquinas.
+        //Borde superior, 1<i<N y j=0
+        for(i=1;i<N-1;i++){
+            mSecConvergido[i*N] = ((mSec[(i-1)*N] + mSec[i*N] + mSec[(i+1)*N] + mSec[(i-1)*N+1] + mSec[i*N+1] + mSec[(i+1)*N+1]) / 6.0);
+        }
+        //Borde inferior, 1<i<N y j=N-1
+        for(i=1;i<N-1;i++){
+            mSecConvergido[i*N+N-1] = ((mSec[(i-1)*N+N-2] + mSec[i*N+N-2] + mSec[(i+1)*N+N-2] + mSec[(i-1)*N+N-1] + mSec[i*N+N-1] + mSec[(i+1)*N+N-1]) / 6.0);
+        }
+        //Borde izquierdo, i=0, 1<j<N
+        for(j=1;j<N-1;j++){
+            mSecConvergido[j] = ((mSec[j-1] + mSec[j] + mSec[j+1] + mSec[N+j-1] + mSec[N+j] + mSec[N+j+1]) / 6.0);
+        }
+        //Borde derecho, i=N-1, 1<j<N
+        for(j=1;j<N-1;j++){
+            mSecConvergido[(N-1)*N+j] = ((mSec[(N-2)*N+j-1] + mSec[(N-2)*N+j] + mSec[(N-2)*N+j+1] + mSec[(N-1)*N+j-1] + mSec[(N-1)*N+j] + mSec[(N-1)*N+j+1]) / 6.0);
+        }
+
+        //Luego se calcula el promedio de la matriz interna, para 1<i<N-1, 1<j<N-1
+        for(i=1;i<N-1;i++){
+            for(j=1;j<N-1;j++){
+                mSecConvergido[i*N+j]=(
+                    mSec[(i-1)*N+j-1] +
+                    mSec[(i)  *N+j-1] +
+                    mSec[(i+1)*N+j-1] +
+                    mSec[(i-1)  *N+j] +
+                    mSec[(i)    *N+j] +
+                    mSec[(i+1)  *N+j] +
+                    mSec[(i-1)*N+j+1] +
+                    mSec[(i)  *N+j+1] +
+                    mSec[(i+1)*N+j+1]
+                ) / 9.0 ;
             }
-            //printf("%.2f\n", mSecConvergido[i]); TODO: borrar
 	    }
 
-        //Se evalúa todos los valores del vector convergieron. Se saltea la primera posición dado que esa se toma como el valor de convergencia.
-        convergio = true;
-        for(i=1;i<N;i++){
-            //TODO: el valor de precision está hardcodeado, cambiar
-            //Si algún valor no estuviera por debajo del límite de precisión, se copia el valor del vector actual al original
-            //printf("v[0]: %.2f\n", fabs(mSecConvergido[0] - mSecConvergido[i]));
-            if(!(fabs(mSecConvergido[0] - mSecConvergido[i])<0.1)){
-                convergio = false;
-                //TODO: Hay alguna manera mejor que usar memcpy?
-                memcpy(mSecOri, mSecConvergido, numBytes);
+        //para debugear, borrar despues
+        if(firstTime){
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                printf("mSecConv[%i,%i]: %.2f\n", i, j, mSecConvergido[i*N+j]);
             }
         }
-        //printf("v[0]: %.2f\n", mSecConvergido[0]);
+        firstTime = false;}
+        
+
+        //Se evalúa todos los valores del vector convergieron.
+        convergio = true;
+        mSec[0] = mSecConvergido[0];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                //printf("m[%i]: %.2f\n",i*N+j, mSecConvergido[i*N+j]);
+                mSec[i*N+j] = mSecConvergido[i*N+j];
+                //Si algún valor no estuviera por debajo del límite de precisión, se copia el valor del vector actual al original
+                //printf("m[%lu]: %.2f\n",i*N+j, fabs(mSecConvergido[0] - mSecConvergido[i*N+j]));
+                if(!(fabs(mSecConvergido[0] - mSecConvergido[i*N+j])<0.01)){
+                    convergio = false;
+                }
+            }
+        }
     }
-    printf("Convergio al valor: %.2f en %i iteraciones\n", mSecConvergido[0], iteraciones);
+    printf("Convergio al valor: %.2f en %i iteraciones que tomaron %f segundos\n", mSecConvergido[0], iteraciones, dwalltime() - timetick);
+
+
+    //SOLO SE PARALELIZA EL FOR EXTERIOR
+
 
     return 0;
 }
